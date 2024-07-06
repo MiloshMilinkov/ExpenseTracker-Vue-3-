e4cfee3fc39214861019ae95de6dc79d99866623
Milos Milinkov <milos300399@gmail.com>
2024-07-06 22:36:09 +0200

    Add new Transaction

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <Header />
   <div class="container">
-    <Balance :total = "total" />
+    <Balance :total = "+total" />
     <IncomeExpenses :transactions = "transactions" />
     <TransactionLists :transactions = "transactions" />
-    <AddTransactions/>
+    <AddTransactions @transaction-submited="addTransaction" />
   </div>
 </template>
 
@@ -15,6 +15,9 @@
   import TransactionLists from './components/TransactionLists.vue';
   import AddTransactions from './components/AddTransactions.vue';
   import { ref, computed } from 'vue';
+  import { useToast } from 'vue-toastification';
+
+  const toast = useToast();
 
   const transactions = ref ([ 
     {id: 1, text: 'Flower', amount: -19.99},
@@ -28,12 +31,24 @@
     return transactions.value.reduce((acc, transaction) =>{
       return acc + transaction.amount;
     }, 0);
-  })
+  });
 
 
+  //addTransaction
+  const addTransaction = (transactionData) =>{
+    transactions.value.push({
+      id: generateUniqueId(),
+      text: transactionData.text,
+      amount: transactionData.amount
+    });
+
+    toast.success('Transaction added.')
+  };
+
+  //Generate id
+  const generateUniqueId = () => {
+    return Math.floor(Math.random() * 1000);
+  }
 
 </script>
 
-<style scoped>
-
-</style>
--- src/components/AddTransactions.vue
+++ src/components/AddTransactions.vue
@@ -1,18 +1,56 @@
 <template>
     <div>
         <h3>Add new transaction</h3>
-        <form id="form">
+        <form id="form" @submit.prevent="onSubmit">
             <div class="form-control">
                 <label for="text">Text</label>
-                <input type="text" id="text" placeholder="Enter text...">
+                <input type="text" id="text" placeholder="Enter text..." v-model="text">
             </div>
             <div class="form-control">
                 <label for="amount">
                     Amount <br> (negative - epxense, positive - income)
                 </label>
-                <input type="number" id="amount" placeholder="Enter amount...">
+                <input type="text" id="amount" placeholder="Enter amount..." v-model="amount">
             </div>
             <button class="btn">Add transaction</button>
         </form>
     </div>
 </template>
+
+<script setup>
+    import { ref } from 'vue';
+    import { useToast } from 'vue-toastification';
+
+
+    const text = ref('');
+    const amount = ref('');
+
+    const toast = useToast();
+
+    const onSubmit = () =>{
+        if( !text.value && !amount.value){
+            toast.error('Both fields must be filled.');
+            return;
+        }
+        else if( !amount.value){
+            toast.error('Amount field must be filled.');
+            return;
+        }
+        else if( !text.value ){
+            toast.error('Text field must be filled.');
+            return;
+        }
+
+        const transactionData = {
+            text: text.value,
+            amount: parseFloat(amount.value)
+        }
+
+        emit('transactionSubmited', transactionData);
+        
+        text.value = '';
+        amount.value = '';
+    }
+
+    const emit = defineEmits(['transactionSubmited']);
+</script>
