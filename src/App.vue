a1090df9375ceb83987870068c682563d6cad441
Milos Milinkov <milos300399@gmail.com>
2024-07-06 17:48:36 +0200

    Reactive Transaction List

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
   <div class="container">
     <Balance />
     <IncomeExpenses/>
-    <TransactionLists/>
+    <TransactionLists :transactions = "transactions" />
     <AddTransactions/>
   </div>
 </template>
@@ -14,9 +14,17 @@
   import IncomeExpenses from './components/IncomeExpenses.vue';
   import TransactionLists from './components/TransactionLists.vue';
   import AddTransactions from './components/AddTransactions.vue';
+  import { ref } from 'vue';
+
+  const transactions = ref ([ 
+    {id: 1, text: 'Flower', amount: -19.99},
+    {id: 2, text: 'Beer', amount: -4.99},
+    {id: 3, text: 'Paycheck', amount: +540.00}
+  ]);
+    
 
 </script>
 
-<style  scoped>
+<style scoped>
 
 </style>
